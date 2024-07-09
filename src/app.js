1dd687c80cd85c9d5c07f8af28b8c0747c915014
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-09 23:33:38 +0700

    feat: config env

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
+require("dotenv").config();
+
 const app = express();
 
 //init middleware
@@ -11,8 +13,6 @@ app.use(compression());
 
 //init db
 require("./dbs/init.mongodb");
-const { checkOverload } = require("./helpers/check.connect");
-checkOverload();
 
 //init routes
 app.get("/", (req, res) => {
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -2,8 +2,12 @@
 
 const mongoose = require("mongoose");
 const { countConnect } = require("../helpers/check.connect");
+const {
+  db: { host, port, name },
+} = require("../configs/config.mongodb");
 
-const connectString = `mongodb://localhost:27017/shopDEV`;
+const connectString = `mongodb://${host}:${port}/${name}`;
+console.log(connectString);
 
 class Database {
   constructor() {
