bcd29392b4428817a396f2e12a9ac9b86895902b
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-10 23:55:30 +0700

    feat: custom dynamic middleware for apikey and permissions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 "use strict";
 
 const exppress = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
 const router = exppress.Router();
+
+//Check apikey
+router.use(apiKey);
+//Check permission
+router.use(permission("0001"));
 
 router.use("/v1/api", require("./access"));
 // router.get("", (req, res) => {
---
+++ src/services/apiKey.service.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const apiKeyModel = require("../models/apiKey.model");
+const crypto = require("crypto");
+
+const findById = async (key) => {
+  const newObj = await apiKeyModel.create({
+    key: crypto.randomBytes(64).toString("hex"),
+    permissions: "000",
+  });
+  const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
+  return objKey;
+};
+
+module.exports = { findById };
